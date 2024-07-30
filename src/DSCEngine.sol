385ea6b227638a6cd1eed0c4bc50ea57cf0bbe7c
Vention Young <backstreetal6year@gmail.com>
2024-07-30 03:00:51 +0000

    test commit

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -5,6 +5,9 @@ contract DSCEngine {
 
     error DSCEng_NoZeroTxPlease();
 
+    //State mapping for allow list of token
+    mapping (address => bool) s_tokenToAllowed;
+
     modifier NoZeroTx (uint256 amount) {
         if (amount ==0) {
             revert DSCEng_NoZeroTxPlease();
@@ -13,14 +16,15 @@ contract DSCEngine {
     }
 
     modifier IsSupportedToken (address token) {
+        //if the token is not in the allow list then revert
         
-
+        _;
     }
 
     constructor() {}
 
 
-    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral) external modifier NoZeroTx(amountCollateral) {
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral) external NoZeroTx(amountCollateral) {
         
     }
 
