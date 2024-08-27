fe044ea0f64598c23548e2edcd2e9e6c9308c3dc
Vention Young <backstreetal6year@gmail.com>
2024-08-27 03:18:13 +0000

    add deposit test, faile get value on spolia test

--- test/uint/DSCEngineTest.t.sol
+++ test/uint/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
 
@@ -19,10 +20,17 @@ contract DSCEngineTest is Test {
     //address btcUsdPriceFeed;
     //address wbtc;
 
+    address public USER = makeAddr("user");
+
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, helperConfig) =  deployer.run();
-        (ethUsdPriceFeed, ,weth , , ) = helperConfig.activeNetworkConfig();    }
+        (ethUsdPriceFeed, ,weth , , ) = helperConfig.activeNetworkConfig();   
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+         
+         }
 
     //the most important, test for get the usd value for the token
     //price test
@@ -34,4 +42,17 @@ contract DSCEngineTest is Test {
         uint256 actualUsdValue = dsce.getUSDValueForToken(weth, ethAmount);
         assertEq(actualUsdValue, expectedUsdValue);
     }
+
+
+    //dipositCollateral test
+
+    function testRevertsIfCollateralAmountIsZero() public{
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), 1e18);
+
+        vm.expectRevert(DSCEngine.DSCEng_NoZeroTxPlease.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
 }
